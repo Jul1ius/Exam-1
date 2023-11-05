7bb1ad5ab12a65a82554bf7b2cad56dd74b2a5de
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-11-05 09:03:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    { 
+        int N = int.Parse(Console.ReadLine());
+
+        double totalSum = 0;
+
+        for (int i = 1; i <= N; i++)
+        {
+            double grade = double.Parse(Console.ReadLine());
+            totalSum += grade;
+        }
+
+        double average = totalSum / N;
+
+        Console.WriteLine($"{average:F2}");
+    }
+}
